5fad9d80a33b096292576d37bf29e242efa04c3f
Nasser Aldaghri <79866053+aldaghri@users.noreply.github.com>
2021-03-03 21:56:47 -0500

    Add files via upload

---
+++ compact.sh
@@ -0,0 +1,3 @@
+python3  -u CMU.py --Alpha=0.001 --Code_rate=5  --Num_trials=5000 \
+		--Synthetic=0 --Num_test=692 --Random_proj=0 --D=25\
+		--Show_progress=1
---
+++ synthetic.sh
@@ -0,0 +1,5 @@
+python3  -u CMU.py --Alpha=0 --Code_rate=5  \
+		--Num_trials=2000 --Synthetic=1 \
+		--Sigma=0.5 --Num_features=50 \
+		--Num_samples=15000 --Num_test=2000 \
+		--Random_proj=0 --D=0 --Show_progress=0
